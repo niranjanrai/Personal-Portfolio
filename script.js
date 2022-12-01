810aeb82ffdcf0534f883c38a34609c9c8ce5f24
niranjanrai <rainiranjan1998@gmail.com>
2022-12-02 01:55:51 +0530

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [90, 80, 50, 80, 50, 50, 10];
+const progressBarPercents = [80, 70, 50, 60, 50, 50, 50];
 
 window.addEventListener("scroll", () => {
   mainFn();
